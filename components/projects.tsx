6c7f351ec03963d18455fca90671d760563b36ac
Arthath Agary <arthathagary@gmail.com>
2024-04-12 17:10:28 +0530

    update recent contents

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,6 +5,7 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import Link from "next/link";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
@@ -18,6 +19,31 @@ export default function Projects() {
             <Project {...project} />
           </React.Fragment>
         ))}
+      </div>
+      <div>
+        <Link
+          href="https://github.com/arthathagary?tab=repositories"
+          target="_blank"
+          className="flex justify-center items-center gap-3 mt-6 font-semibold md:text-xl text-base cursor-pointer"
+        >
+          For More Projects Click Here
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-square-arrow-out-up-right"
+          >
+            <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" />
+            <path d="m21 3-9 9" />
+            <path d="M15 3h6v6" />
+          </svg>
+        </Link>
       </div>
     </section>
   );
